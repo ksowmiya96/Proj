45ef17d4e37fd7691ffecb7853c24006bf285558
ksowmiya96 <ksowmiya96@gmail.com>
2022-01-28 22:00:43 +0530

    edited

--- src/test/java/Re.java
+++ src/test/java/Re.java
@@ -4,7 +4,7 @@ public class Re {
 	public static void main(String[] args) {
 		
 		System.out.println("hi");
-		System.out.println("helllllll");
-System.out.println("sowmiya");	}
-
+		System.out.println("jello");
+		System.out.println("hello1");
+}
 }
